e932eee064745308252737d4bcd07a30c9ebad21
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-11-02 17:08:10 -0500

    Rename config module and conf dir

--- src/gameai/config/io.py
+++ src/gameai/config/io.py
@@ -3,18 +3,30 @@ from typing import Any
 
 import yaml
 
-CONF_DIR = pathlib.Path(__file__).parent / "conf_files"
+CONF_DIR = pathlib.Path(__file__).parent / "settings"
+
+__settings = {}
 
 
 # TODO:
 # * create local config file on first run, load from local on subsequent runs
 # * save configuration changes
-def load_config(filename: str) -> Any:
+def load_settings(filename: str) -> Any:
+    if filename in __settings:
+        return __settings[filename]
+
     try:
         with open(CONF_DIR / filename) as f:
-            return yaml.safe_load(f)
+            settings = yaml.safe_load(f)
+            __settings[filename] = settings
+            return settings
+
     except yaml.YAMLError as e:
         # TODO: log yaml load error
         print(f"Failed to read YAML from {filename}: {e}")
     except IOError as e:
         print(f"IO error reading {filename}: {e}")
+
+
+def save_settings():
+    pass
